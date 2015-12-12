39996878fa7eb63f62a70a4efcefd33c95897795
Jeffrey Wan <Jwan622@gmail.com>
2015-12-11 19:14:17 -0500

    adding 12 and 13

---
+++ exercise_13.py
@@ -0,0 +1,13 @@
+# here we import the argv module
+
+# run this script by typing python exercise_13.py first 2nd 3rd
+
+from sys import argv
+
+script, first, second, third = argv
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+print argv
